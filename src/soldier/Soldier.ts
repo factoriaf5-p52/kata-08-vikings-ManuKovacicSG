350af1f6838da168b0a6f8cb28b941f181341544
ManuKovaSG <kovacicmanu@gmail.com>
2022-12-13 00:11:29 +0100

    comenzada la kata 8, un test pasado

--- src/soldier/Soldier.ts
+++ src/soldier/Soldier.ts
@@ -1,2 +1,14 @@
 export class Soldier {
+    health:number;
+    strength: number;
+
+    constructor(healthArg: number, strengthArg: number) {
+        this.health = healthArg
+        this.strength = strengthArg
+    };
+
+    attack(s:number){
+        return this.strength = s
+    }
+
 }
